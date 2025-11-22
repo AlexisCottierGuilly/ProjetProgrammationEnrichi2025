409221ab072b08f1ede3213800ecfed73b7c18b2
Alexis CG <113210633+AlexisCottierGuilly@users.noreply.github.com>
2025-11-22 18:11:38 -0500

    More settings

--- interface.py
+++ interface.py
@@ -17,11 +17,14 @@ import time
 Functionalities
  - Place points (1, 2 = blue, red) ✅
  - Create polygon (space) ✅
- - Set seed (input field)
+ - Set seed (input field) ✅
  - Set points (input field) ✅
  - Step by step (next) ✅
- - Random points mode (with r)
- - Move points
+ - Random points mode (with r) ✅
+ - Move points ✅
+ - Save dataset ✅
+ - Load dataset ✅
+ - Set number of points
 """
 
 mpl.rcParams.update({
@@ -54,7 +57,6 @@ class PolygonInterface:
         self.draggable_point = None
         self.draggable_point_index = None
 
-        self.auto_step = True
         self.step_delay = 0  #0.01
 
         self.fig = plt.figure(facecolor="#101010")
@@ -72,10 +74,18 @@ class PolygonInterface:
         self.reset_mode_callbacks = []
         self.seed_callbacks = []
         self.constraint_callbacks = []
+        self.step_mode_callbacks = []
+        self.nb_pts_callbacks = []
+        self.random_seed_callbacks = []
 
         self.setup_buttons()
 
-        self.num_blue, self.num_red = 10, 10
+        self.num_blue, self.num_red = None, None
+        self.set_number_of_points(10, 10)  # Update UI
+
+        self.auto_step = None
+        self.set_auto_step(True)  # Update UI
+
         self.set_random_points()
 
         self.create_polygon()
@@ -106,15 +116,8 @@ class PolygonInterface:
         interface_utils.customize_button(reset_button, mpl.rcParams)
         self.buttons.append(reset_button)
 
-        step_y = y_step + vert_spacing / 1.25
-        step_ax = self.fig.add_axes([x_start, step_y, bar_width, box_height])  #[0.7, 0.05, 0.1, 0.075])
-        step_button = Button(step_ax, "Step")
-        step_button.on_clicked(lambda event: (self.step()))
-        interface_utils.customize_button(step_button, mpl.rcParams)
-        self.buttons.append(step_button)
-
         # Add point textbox
-        add_point_y = step_y + vert_spacing / 1.25
+        add_point_y = y_step + vert_spacing / 1.25
         textbox_width = bar_width / 1.75
         add_point_ax = self.fig.add_axes([x_start, add_point_y, textbox_width, box_height])
         add_point_textbox = interface_utils.InterfaceTextBox(add_point_ax, "")
@@ -187,7 +190,14 @@ class PolygonInterface:
         # LEFT SIDE
         x_center = 0.15
         x_start = x_center - bar_width / 2
-        modes_y = y_step
+
+        step_ax = self.fig.add_axes([x_start, y_step, bar_width, box_height])  # [0.7, 0.05, 0.1, 0.075])
+        step_button = Button(step_ax, "Step")
+        step_button.on_clicked(lambda event: (self.step()))
+        interface_utils.customize_button(step_button, mpl.rcParams)
+        self.buttons.append(step_button)
+
+        modes_y = y_step + vert_spacing / 1.25
 
         gen_mode_width = bar_width / 2.2
         gen_mode_ax = self.fig.add_axes([x_start, modes_y, gen_mode_width, box_height])  # [0.7, 0.05, 0.1, 0.075])
@@ -198,7 +208,7 @@ class PolygonInterface:
         gen_mode_button.on_clicked(lambda event: self.gen_mode_clicked(gen_mode_button))
         interface_utils.customize_button(gen_mode_button, mpl.rcParams)
         self.buttons.append(gen_mode_button)
-        self.fig.text(x_start + gen_mode_width / 2, modes_y + 0.1, "Reset Mode", ha="center", va="center")
+        self.fig.text(x_start + gen_mode_width / 2, modes_y + 0.1, "Data Mode", ha="center", va="center")
 
         constraint_mode_width = gen_mode_width
         space = bar_width - constraint_mode_width - gen_mode_width
@@ -217,9 +227,38 @@ class PolygonInterface:
             "Constraint", ha="center", va="center"
         )
 
-        # Save file
-        seed_y = modes_y + vert_spacing
-        seed_ax = self.fig.add_axes([x_start, seed_y, bar_width, box_height])
+        auto_step_y = modes_y + vert_spacing
+        auto_step_width = bar_width / 2.2
+        auto_step_ax = self.fig.add_axes(
+            [x_start, auto_step_y, auto_step_width, box_height])  # [0.7, 0.05, 0.1, 0.075])
+        auto_step_button = Button(auto_step_ax, "AUTO")
+        self.step_mode_callbacks.append(
+            lambda new_mode: auto_step_button.label.set_text(self.get_step_mode_string())
+        )
+        auto_step_button.on_clicked(lambda event: self.auto_step_clicked(auto_step_button))
+        interface_utils.customize_button(auto_step_button, mpl.rcParams)
+        self.buttons.append(auto_step_button)
+        self.fig.text(x_start + auto_step_width / 2, auto_step_y + 0.1, "Step Mode", ha="center", va="center")
+
+        nb_pts_width = auto_step_width
+        space = bar_width - nb_pts_width - auto_step_width
+        s = x_start + auto_step_width + space
+
+        nb_pts_ax = self.fig.add_axes([s, auto_step_y, nb_pts_width, box_height])
+        nb_pts_textbox = interface_utils.InterfaceTextBox(nb_pts_ax, "")
+        nb_pts_textbox.on_text_change(lambda text: self.update_nb_pts_text(nb_pts_textbox))
+        nb_pts_textbox.submit_function = lambda: self.on_nb_pts_submit(nb_pts_textbox)
+        self.nb_pts_callbacks.append(
+            lambda nb_b, nb_r: nb_pts_textbox.set_text(str(nb_b + nb_r))
+        )
+        interface_utils.customize_button(nb_pts_textbox, mpl.rcParams)
+        self.textboxes.append(nb_pts_textbox)
+
+        self.fig.text(s + nb_pts_width / 2, auto_step_y + 0.1, "Nb Points", ha="center", va="center")
+
+        seed_y = auto_step_y + vert_spacing
+        seed_width = bar_width / 1.5
+        seed_ax = self.fig.add_axes([x_start, seed_y, seed_width, box_height])
         seed_textbox = interface_utils.InterfaceTextBox(seed_ax, "")
         seed_textbox.on_text_change(lambda text: self.update_seed_text(seed_textbox))
         seed_textbox.submit_function = lambda: self.on_seed_submit(seed_textbox)
@@ -229,7 +268,22 @@ class PolygonInterface:
         interface_utils.customize_button(seed_textbox, mpl.rcParams)
         self.textboxes.append(seed_textbox)
 
-        self.fig.text(x_start + bar_width / 2, seed_y + 0.1, "Seed", ha="center", va="center")
+        self.fig.text(x_start + seed_width / 2, seed_y + 0.1, "Seed", ha="center", va="center")
+
+        rnd_seed_width = bar_width / 4
+        space = bar_width - rnd_seed_width - seed_width
+        s = x_start + seed_width + space
+
+        seed_rnd_ax = self.fig.add_axes([s, seed_y, rnd_seed_width, box_height])
+        seed_rnd_button = Button(seed_rnd_ax, "YES")
+        seed_rnd_button.on_clicked(lambda event: self.random_seed_clicked(seed_rnd_button))
+        interface_utils.customize_button(seed_rnd_button, mpl.rcParams)
+        self.random_seed_callbacks.append(
+            lambda rnd: seed_rnd_button.label.set_text("YES" if rnd else "NO")
+        )
+        self.buttons.append(seed_rnd_button)
+
+        self.fig.text(s + rnd_seed_width / 2, seed_y + 0.1, "Random", ha="center", va="center")
 
     def set_constraint(self, constraint):
         self.constraint = constraint
@@ -246,8 +300,24 @@ class PolygonInterface:
         for callback in self.seed_callbacks:
             callback(seed)
 
+    def set_number_of_points(self, blues, reds):
+        self.num_blue = blues
+        self.num_red = reds
+        for callback in self.nb_pts_callbacks:
+            callback(blues, reds)
+
+    def set_auto_step(self, state):
+        self.auto_step = state
+        for callback in self.step_mode_callbacks:
+            callback(state)
+
+    def set_random_seed(self, state):
+        self.random_seed = state
+        for callback in self.random_seed_callbacks:
+            callback(state)
+
     def show_notification(self, text):
-        notification = self.fig.text(0.5, 0.1, text,
+        notification = self.fig.text(0.515, 0.07, text,
                        ha="center", va="center", color="grey")
 
         timer = self.fig.canvas.new_timer(interval=3000)
@@ -260,9 +330,20 @@ class PolygonInterface:
     def get_reset_mode_string(self):
         return "RANDOM" if self.reset_mode == RESET_RANDOM else "DATASET"
 
+    def get_step_mode_string(self):
+        return "AUTO" if self.auto_step else "MANUAL"
+
+    def random_seed_clicked(self, button):
+        self.set_random_seed(not self.random_seed)
+        plt.draw()
+
+    def auto_step_clicked(self, button):
+        self.set_auto_step(not self.auto_step)
+        self.initialize_polygon()
+        self.update_graphics()
+
     def constraint_mode_clicked(self, button):
         self.switch_constraint_mode()
-        button.label.set_text(self.get_constraint_string())
         self.initialize_polygon()
         self.update_graphics()
 
@@ -296,6 +377,23 @@ class PolygonInterface:
             self.set_reset_mode(RESET_RANDOM)
             self.random_seed = False
             self.seed = int(seed_text)
+            self.reset()
+
+    def update_nb_pts_text(self, textbox):
+        if textbox.setting_text:
+            return
+
+        accepted_characters = "0123456789"
+        accepted_string = "".join([char for char in textbox.text if char in accepted_characters])
+        textbox.set_text(accepted_string)
+
+    def on_nb_pts_submit(self, textbox):
+        nb_pts_text = textbox.text
+        if nb_pts_text != "":
+            nb = int(nb_pts_text)
+            self.num_red = nb // 2
+            self.num_blue = nb - self.num_red
+            self.set_reset_mode(RESET_RANDOM)
             self.reset()
 
     def update_load_text(self, textbox):
@@ -558,7 +656,7 @@ class PolygonInterface:
 
     def set_random_points(self):
         if self.random_seed:
-            self.set_seed(random.randint(1, 1_000_000_000_000_000))
+            self.set_seed(random.randint(1, 1_000_000_000_000))
         self.points = poly_gen.get_random_points(self.seed, self.num_blue, self.num_red)
         self.update_points()
 
@@ -632,7 +730,9 @@ class PolygonInterface:
         else:
             title = f"Dataset '{self.current_dataset}'"
 
-        self.ax.set_title(f"{title}\nP: {peri:.3f} u | A: {area:.3f} u^2")
+        subtitle = f"P: {peri:.3f} u | A: {area:.3f} u^2"
+        self.ax.set_title(f"{title}")
+        self.ax.set_xlabel("\n" + subtitle)
 
     def update_graphics(self):
         if self.polygon is not None:
